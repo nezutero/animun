ece7db9b680e55adcee167e9056e92d56666bf98
amodotomi <amodotomi@gmail.com>
2023-07-25 22:07:16 +0300

    feat: implement data fetching

--- api/api.go
+++ api/api.go
@@ -1,9 +1,52 @@
 package api
 
 import (
-
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func GetData() {
+const QUERY = `
+query ($id: Int) {
+  Media (id: $id, type: ANIME) {
+    id
+    title {
+      romaji
+      english
+      native
+    }
+  }
+}`
 
+func GetData() {
+	queryData := map[string]interface{}{
+		"query":     QUERY,
+		"variables": map[string]int{"id": 15125},
+	}
+
+	jsonData, err := json.Marshal(queryData)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.Post("https://graphql.anilist.co/", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%#v\n", result)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module main.go
 
 go 1.20
+
+require (
+	github.com/enescakir/emoji v1.0.0
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
